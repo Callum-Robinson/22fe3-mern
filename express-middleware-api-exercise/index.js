c12d00d8d4b4a554190f2e72e9011cb305c4300a
MrWalshyType2 <mwalsh@qa.com>
2022-05-05 11:01:07 +0100

    Added env example

--- express-middleware-api-exercise/index.js
+++ express-middleware-api-exercise/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const userRouter = require('./routes/user-router');
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+if (process.env.NODE_ENV === "PRODUCTION") {
+    console.log("=== PRODUCTION ===");
+    app.use(morgan('combined'));
+} else {
+    console.log("=== DEVELOPMENT ===");
+    app.use(morgan('dev'));
+}
+
 // 1. Add a request logger to the beginning of the middleware chain
 //    - create a function which accepts (request, response, next) and logs info, then calls next()
 //    - apply the middleware with app.use()
+// Optional task: Try adding the 'morgan' logger to your application
+// - developed by Express
+// - https://github.com/expressjs/morgan
+// - Scroll down to Examples after running 'npm i morgan', see the 
+//   express/connect example
 
 // middleware
 // express.json() middleware uses the body-parser dependency (included in Express) to parse JSON request bodies
